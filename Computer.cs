2731cdea103bf1c6861b75a66d451e5cbf6cde0e
JTLeon301 <jtleon301@comcast.net>
2024-02-14 00:53:36 -0600

    adding assignment to repository

---
+++ Computer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilder
+{
+    internal class Computer
+    {
+        //properties
+        public string ProcessorType { get; set; }
+        public string GraphicsCard { get; set; }
+        public double RAMSize { get; set; }
+        public double StorageSize { get; set; }
+
+        //default constructor method
+        public Computer()
+        {
+            //initialize the properties to a default value
+            ProcessorType = "not selected";
+            GraphicsCard = "not selected";
+            RAMSize = 0;
+            StorageSize = 0;
+        }
+
+        //non-default constructor method
+        public Computer(string processorType, string graphicsCard, double ramSize, double storageSize)
+        {
+            ProcessorType = processorType;
+            GraphicsCard = graphicsCard;
+            RAMSize = ramSize;
+            StorageSize = storageSize;
+        }
+
+        //ToString method
+        public override string ToString()
+        {
+            return $"Processor: {ProcessorType}, Graphics Card: {GraphicsCard}, RAM: {RAMSize}GB, StorageCapacity: {StorageSize}GB";
+        }
+
+        //method to calculate the price of the user's PC build
+        public double CalculateCost()
+        {
+            //applying average market prices to each component
+            double processorPrice = 300;
+            double graphicsCardPrice = 650;
+            double ramPricePerGB = 0.80;
+            double storagePricePerGB = 0.12;
+
+            //expression to calculate the total cost of the user's computer
+            double totalBuildCost = processorPrice + graphicsCardPrice + (RAMSize * ramPricePerGB) + (StorageSize * storagePricePerGB);
+            return totalBuildCost;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using static System.Console;
+namespace ComputerBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Name: Jaiden Leonard
+             * Class: .NET Programming
+             * Date: February 13th, 2024
+             * 
+             * Program:
+             * This program aims to let the user enter the components that they would like in their PC (processor, 
+             * graphics card, ram size, and storage size) and gives the user back what they entered and also the
+             * total cost of the PC based on average market pices for each component.
+             * 
+             * Struggles:
+             * None really, the walkthrough in class made this assignment pretty simple.
+             * 
+             * NOTE:
+             * If you notice anything I could improve on or change, feel free to leave a comment on the assignment
+             * grading page. All feedback positive or negative is welcomed. Thank you!
+             */
+
+            //print application header
+            WriteLine("*************************************");
+            WriteLine("       Computer Builder V1.0");
+            WriteLine("*************************************");
+
+            //initialize variables
+            string again;
+
+            //do while loop to run the program
+            do
+            {
+                WriteLine("\nWhat does your dream PC entail?");
+
+                //ask the user what processor they want
+                Write("What processor would you like?: ");
+                string processorType = ReadLine();
+
+                //ask the user what graphics card they want
+                Write("What graphics card would you like?: ");
+                string graphicsCard = ReadLine();
+
+                //ask the user how many GBs of RAM they want
+                Write("How many GB of RAM would you like?: ");
+                double ramSize = double.Parse(ReadLine());
+
+                //ask the user how many GBs of storage they want
+                Write("How many GB of storage would you like?: ");
+                double storageSize = double.Parse(ReadLine());
+
+                Computer dreamBuild = new Computer(processorType, graphicsCard, ramSize, storageSize);
+
+                //print the PC's specs to the user
+                WriteLine("\nHere's what your dream PC's specs look like!");
+                WriteLine(dreamBuild);
+
+                //print the total cost of the build to the user
+                WriteLine($"\nTotal build cost: {dreamBuild.CalculateCost():C}");
+
+                //ask the user if they want to build another PC
+                Write("Would you like to build another computer? (y/n): ");
+                again = ReadLine().Trim().ToLower();
+            } while (again == "y");
+
+            //program termination message
+            WriteLine("\nThank you for using ComputerBuilder!");
+        }
+    }
+}
